7105dd0d5dad36bfdfd0060c8dd417927f170a68
Cameron Strandberg <cameron.strandberg@trustflight.com>
2024-02-24 16:27:30 -0800

    testing aws release

--- apps/homepage/src/app/app.tsx
+++ apps/homepage/src/app/app.tsx
@@ -2,12 +2,60 @@
 import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
+// importing the component from the library
+import { SomeList } from '@react-monorepo/shared-ui';
 
 export function App() {
   return (
-    <div>
-      LETS MAKE A CHANGE RIGHT HERE.
-      <NxWelcome title="homepage" />
+    <div className="min-h-screen bg-[#150f1c] lato-regular p-5">
+      <div className="mx-auto max-w-4xl min-h-screen flex text-white ">
+        <div className="w-1/4 p-2">
+          <img
+            src="/src/assets/profile.png"
+            alt="profile"
+            className="rounded-full border-4 border-white-500"
+          />
+        </div>
+        {/* w-1/4 sets the width to 25% of the parent */}
+        <div className="w-3/4 p-4">
+          <div className="py-4">
+            <h1 className="text-xl">👋 Hi! I'm Cameron</h1>
+          </div>
+          <div className="py-4">
+            <p className="text-base">I build awesome software.</p>
+          </div>
+          <div className="py-4">
+            <p className="text-base">
+              For enquires,{' '}
+              <a
+                href="https://calendly.com/cameron-strandberg"
+                className="underline text-yellow-300 font-bold hover:text-indigo-900 hover:bg-yellow-400"
+              >
+                please book something on my calendar.
+              </a>
+              <br />
+              or email me at{' '}
+              <strong>cameron.strandberg[at]gmail[dot]com</strong>
+            </p>
+          </div>
+
+          <div className="py-4">
+            <p className="text-base">
+              Here is my{' '}
+              <a
+                href="/src/assets/Cameron Strandberg - Software Engineer Resume.pdf"
+                className="underline text-yellow-300 font-bold hover:text-indigo-900 hover:bg-yellow-400"
+                download
+              >
+                resume
+              </a>
+              .
+            </p>
+          </div>
+          <div className="py-4"></div>
+        </div>{' '}
+        {/* w-3/4 sets the width to 75% of the parent */}
+      </div>
     </div>
   );
 }
